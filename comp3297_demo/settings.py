fb17b472630c35fcc4d9fabee9c80ceaa0b675dd
Tony Mok <tonymok@Tonys-MacBook-Pro.local>
2020-10-07 01:49:22 +0800

    add missing file

--- comp3297_demo/settings.py
+++ comp3297_demo/settings.py
@@ -37,6 +37,12 @@ ALLOWED_HOSTS = []
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Application definition
 
