63e6f8b6fd91a49c870c3ca5e2215549cedc5ecb
Tony Mok <tonymok@Tonys-MacBook-Pro.local>
2020-10-07 01:35:12 +0800

    add missing file

---
+++ tutoria/apps.py
@@ -0,0 +1,10 @@
+# uncompyle6 version 3.5.0
+# Python bytecode 2.7 (62211)
+# Decompiled from: Python 2.7.5 (default, Aug  7 2019, 00:51:29) 
+# [GCC 4.8.5 20150623 (Red Hat 4.8.5-39)]
+# Embedded file name: /home/ubuntu/workspace/tutoria/apps.py
+# Compiled at: 2017-10-20 12:10:43
+from django.apps import AppConfig
+
+class TutoriaConfig(AppConfig):
+    name = 'tutoria'
